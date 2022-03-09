273a66a3146be0297656a8a0bfc823b20c07e5a0
Busna27 <lucabusnelli2001@gmail.com>
2022-03-09 14:59:32 +0100

    iniz. Our team

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// ! Imported array
+// ! DON'T TOUCH IT
+const team = [
+    {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+    },
+    {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
